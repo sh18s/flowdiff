3345f5a0a3af23767a15df030a0a5950ca6fc7f3
shoko sawada <g1220522@is.ocha.ac.jp>
2015-12-05 19:15:54 +0900

    add BinarySerach.java

---
+++ flowdiff/src/ocha/itolab/flowdiff/core/seedselect/BinarySearch.java
@@ -0,0 +1,10 @@
+package ocha.itolab.flowdiff.core.seedselect;
+import java.util.Comparator;
+
+public class BinarySearch{
+
+	public LinkedList<RankValue> binarySearch(LinkedList<RankValue> rankList, RankValue key){
+		System.out.println('a');
+		return rankList;
+	}
+}

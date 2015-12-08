b9569060aa2238c47987ebbf6f89449c83f7cefc
shoko sawada <g1220522@is.ocha.ac.jp>
2015-12-08 23:28:43 +0900

    add binary search algorithm

--- flowdiff/src/ocha/itolab/flowdiff/core/seedselect/BinarySearch.java
+++ flowdiff/src/ocha/itolab/flowdiff/core/seedselect/BinarySearch.java
@@ -1,10 +1,30 @@
 package ocha.itolab.flowdiff.core.seedselect;
 import java.util.Comparator;
+import java.util.LinkedList;
+
+import ocha.itolab.flowdiff.core.seedselect.StreamlineDifferenceEvaluator.RankValue;
+
 
 public class BinarySearch{
 
-	public LinkedList<RankValue> binarySearch(LinkedList<RankValue> rankList, RankValue key){
-		System.out.println('a');
+	public static LinkedList<RankValue> main(LinkedList<RankValue> rankList, RankValue key){
+		int pLeft = 0;
+		int pRight = rankList.size() -1;
+		
+		do{
+			int center = (pLeft + pRight) / 2;
+			// 一致するものがあるか確認
+			if(rankList.contains(key.distance)){
+				rankList.add(rankList.lastIndexOf(key) + 1, key);
+				return rankList;
+			// なければ二分探索
+			}else if(rankList.get(center).distance < key.distance){
+				pLeft = center + 1;
+			}else{
+				pRight = center - 1;
+			}
+		}while(pLeft < pRight);
+
 		return rankList;
 	}
 }
